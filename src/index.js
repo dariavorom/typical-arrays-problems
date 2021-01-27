cddfb8b2eb262ff8ffc0b60802e234be779bb3c0
dariavorom <dariavorom@gmail.com>
2021-01-27 20:44:21 +0300

    feat: implement max, min, and avg functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,27 @@
 
 exports.min = function min (array) {
-  return 0;
+    if (!array || (array.length === 0)) {
+        return 0;
+    }
+    return array.reduce(function (a, b) {
+        return ( a < b ? a : b );
+    }, 0);
 }
-
 exports.max = function max (array) {
-  return 0;
+    if (!array || (array.length === 0)) {
+        return 0;
+    }
+  return array.reduce(function (a, b) {
+      return ( a > b ? a : b );
+  }, 0);
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    if (!array || (array.length === 0)) {
+        return 0;
+    }
+    return array.reduce(function (a, b) {
+        return a + b;
+    }, 0) / array.length;
 }
+
